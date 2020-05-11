10eb0449ab6e53c51ee725b6bad784bfebd15aa7
potykion <potykion@gmail.com>
2020-05-11 13:43:18 +0300

    MyHomePage > stless VideosPage

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(context) => MultiProvider(
         child: MaterialApp(
-          home: MyHomePage(),
+          home: VideosPage(),
           theme: ThemeData(
             primaryColor: Colors.pink[100],
             accentColor: Colors.pink[100],
@@ -25,38 +25,31 @@ class MyApp extends StatelessWidget {
       );
 }
 
-class MyHomePage extends StatefulWidget {
+class VideosPage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<VKBloc, VKState>(
-      builder: (context, state) => Scaffold(
-        appBar: AppBar(
-          title: Text(
-            state.accessToken == null
-                ? "Для начала войди в ВК"
-                : state.videoQuery ?? 'Нажми "Загрузить видео"',
+  Widget build(BuildContext context) => BlocBuilder<VKBloc, VKState>(
+        builder: (context, state) => Scaffold(
+          appBar: AppBar(
+            title: Text(
+              state.accessToken == null
+                  ? "Для начала войди в ВК"
+                  : state.videoQuery ?? 'Нажми "Загрузить видео"',
+            ),
+            centerTitle: true,
           ),
-          centerTitle: true,
+          body: state.accessToken != null
+              ? buildVideoListView()
+              : buildVKAuthButton(),
+          floatingActionButton: state.accessToken != null
+              ? FloatingActionButton(
+                  onPressed: () => context
+                      .bloc<VKBloc>()
+                      .add(VKVideoSearchStarted("riley reid")),
+                  child: Icon(Icons.autorenew),
+                )
+              : null,
         ),
-        body: state.accessToken != null
-            ? buildVideoListView()
-            : buildVKAuthButton(),
-        floatingActionButton: state.accessToken != null
-            ? FloatingActionButton(
-                onPressed: () => context
-                    .bloc<VKBloc>()
-                    .add(VKVideoSearchStarted("riley reid")),
-                child: Icon(Icons.autorenew),
-              )
-            : null,
-      ),
-    );
-  }
+      );
 
   Widget buildVKAuthButton() => Builder(
         builder: (context) => Center(
@@ -74,23 +67,21 @@ class _MyHomePageState extends State<MyHomePage> {
       );
 
   Widget buildVideoListView() => BlocBuilder<VKBloc, VKState>(
-        builder: (context, state) => Flexible(
-          child: OrientationBuilder(
-            builder: (context, orientation) =>
-                orientation == Orientation.portrait
-                    ? ListView.builder(
-                        itemBuilder: (context, index) =>
-                            VKVideoCard(video: state.videos[index]),
-                        itemCount: state.videos.length,
-                      )
-                    : GridView.count(
-                        crossAxisCount: 2,
-                        childAspectRatio: 1.75,
-                        children: state.videos
-                            .map((v) => VKVideoCard(video: v))
-                            .toList(),
-                      ),
-          ),
+        builder: (context, state) => OrientationBuilder(
+          builder: (context, orientation) =>
+              orientation == Orientation.portrait
+                  ? ListView.builder(
+                      itemBuilder: (context, index) =>
+                          VKVideoCard(video: state.videos[index]),
+                      itemCount: state.videos.length,
+                    )
+                  : GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.75,
+                      children: state.videos
+                          .map((v) => VKVideoCard(video: v))
+                          .toList(),
+                    ),
         ),
       );
 }
