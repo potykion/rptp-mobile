dc371737781c5ee7fc1c978ca69b3d99e57a95ef
potykion <potykion@gmail.com>
2020-07-08 16:58:01 +0300

    Load actress on settings page

--- lib/core/pages.dart
+++ lib/core/pages.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rptpmobile/actress/blocs.dart';
 import 'package:rptpmobile/actress/widgets.dart';
 import 'package:rptpmobile/core/widgets.dart';
 
 import 'blocs.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.bloc<ActressBloc>().add(DbCheckedEvent());
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: Text("Настроечки")),
